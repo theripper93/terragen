631602bb2e728bb524ee78c5b42dbad463f1ad08
theripper93 <tsplab@gmail.com>
2022-11-18 16:57:36 +0100

    a

--- scripts/locale.js
+++ scripts/locale.js
@@ -1,18 +1,47 @@
-import en from "../translations/en.json" assert {type: 'json'};
+fetch(`../translations/${navigator.language}.json`).then(async (locale) => {
+    const res = await locale.json();
+    initTranslations(res);
+}).catch((err) => {
+    fetch("../translations/en.json").then(async (locale) => {
+        const res = await locale.json();
+        initTranslations(res);
+    });
+})
 
-i18n.translator.add({values: en});
-
-function translate(el) {
-    let innerHtml = el.innerHTML;
-    const toLocalize = innerHtml.matchAll(/{(.*?)}/g);
-    let currentLocal = toLocalize.next();
-    while (!currentLocal.done){
-        const key = currentLocal.value[1];
-        const value = i18n(key);
-        innerHtml = innerHtml.replace(currentLocal.value[0], value);
-        currentLocal = toLocalize.next();
+const flattenObj = (ob) => {
+    let result = {};
+    for (const i in ob) {
+        if ((typeof ob[i]) === 'object' && !Array.isArray(ob[i])) {
+            const temp = flattenObj(ob[i]);
+            for (const j in temp) {
+                result[i + '.' + j] = temp[j];
+            }
+        }
+        else {
+            result[i] = ob[i];
+        }
     }
-    el.innerHTML = innerHtml;
-}
+    return result;
+};
 
-translate(document.body);
+function initTranslations(data){
+    i18n.translator.add({values: flattenObj(data)});
+
+    function translate(el) {
+        let innerHtml = el.innerHTML;
+        const toLocalize = innerHtml.matchAll(/{(.*?)}/g);
+        let currentLocal = toLocalize.next();
+        while (!currentLocal.done){
+            const key = currentLocal.value[1];
+            const value = i18n(key);
+            innerHtml = innerHtml.replace(currentLocal.value[0], value);
+            currentLocal = toLocalize.next();
+        }
+        el.innerHTML = innerHtml;
+    }
+    
+    translate(document.body);
+};
+
+
+
