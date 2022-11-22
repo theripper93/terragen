1c6307e467b97df21904630c9665ac9c132603a7
theripper93 <tsplab@gmail.com>
2022-11-22 14:13:10 +0100

    a

--- scripts/editor/materialManager.js
+++ scripts/editor/materialManager.js
@@ -9,6 +9,9 @@ export class MaterialManager{
         this.materials.push(material);
         const el = this.getElement(material);
         document.querySelector("#texture-panel").appendChild(el);
+        this._materialIndex = this.materials.length - 1;
+        this.selectMaterial();
+        this.updateDisplay();
         return material;
     }
 
@@ -18,6 +21,12 @@ export class MaterialManager{
         const texturePanel = document.querySelector("#texture-panel");
         texturePanel.removeChild(texturePanel.children[index]);
         this.materials.splice(index, 1);
+        const selected = document.querySelector("#texture-panel").querySelector(".active");
+        if(!selected){
+            this._materialIndex = 0;
+            this.selectMaterial();
+        }
+        this.updateDisplay();
     }
 
     currentMaterial(){
@@ -49,6 +58,25 @@ export class MaterialManager{
         return li;
     }
 
+    selectMaterial(){
+        const index = this._materialIndex;
+        if(index < 0 || index >= this.materials.length) return;
+        const texturePanel = document.querySelector("#texture-panel");
+        texturePanel.querySelectorAll("li").forEach((el) => {
+            el.classList.remove("active");
+        });
+        texturePanel.children[index].classList.add("active");
+    }
+
+    updateDisplay(){
+        const texturePanel = document.querySelector("#texture-panel");
+        if(this.materials.length === 0){
+            texturePanel.classList.add("empty");
+        }else{
+            texturePanel.classList.remove("empty");
+        }
+    }
+
     clear(){
         this.materials.forEach((mat) => {
             mat.destroy();
@@ -56,6 +84,7 @@ export class MaterialManager{
         this.materials = [];
         this._materialIndex = 0;
         document.querySelector("#texture-panel").innerHTML = "";
+        this.updateDisplay();
     }
 }
 
