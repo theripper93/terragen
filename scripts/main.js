f3a10116e0e1bef6202593d1e5ff1b96189a8b57
theripper93 <tsplab@gmail.com>
2022-11-22 16:49:18 +0100

    a

--- scripts/main.js
+++ scripts/main.js
@@ -174,4 +174,8 @@ if(canvas.DEBUG) canvas.initProject();
 animate();
 
 
+
+window.onbeforeunload = function() {
+    return "Data will be lost if you leave the page, are you sure?";
+};
 //javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
